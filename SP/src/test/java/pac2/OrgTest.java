ea41c6d538299e4722bda487722f9097092a508e
MRV970 <midderaghavamma@gmail.com>
2024-07-13 11:50:27 +0530

    added

--- SP/src/test/java/pac2/OrgTest.java
+++ SP/src/test/java/pac2/OrgTest.java
@@ -6,6 +6,9 @@ public class OrgTest {
 	@Test
 	public void createOrgTest() {
 		System.out.println("executing createOrgTest");
+		String Url=System.getProperty("url");
+		String Browser=System.getProperty("browser");
+		
 	}
 	@Test
 	public void modifyOrgTest() {
