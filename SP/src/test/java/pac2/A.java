52c30a39eef9431b0f2696bdc8bca6ad2d2a2aa6
MRV970 <midderaghavamma@gmail.com>
2025-07-16 12:55:34 +0530

    commit

---
+++ SP/src/test/java/pac2/A.java
@@ -0,0 +1,16 @@
+package pac2;
+
+public class A {
+	int a;
+	
+	public A(int a) {
+		super();
+		this.a = a;
+	}
+
+	public static void main(String[] args) {
+		A a1=new A(10);
+		System.out.println(a1.a);
+	}
+
+}
